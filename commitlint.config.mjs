08f0270f17d65fe4e3b7c52b04b1a3a64e8f5fab
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-02-16 11:15:59 +0530

    chore: configure husky,prettier and commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,20 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     
+        'fix',      
+        'docs',     
+        'chore',    
+        'style',    
+        'refactor',
+        'ci',       
+        'test'      
+      ]
+    ],
+    'subject-case': [2, 'always', 'lower-case']
+  }
+}
