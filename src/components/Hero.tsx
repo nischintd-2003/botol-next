90781e3df498a3420e11ca8a3e3d255b2d97dfe8
nischintd-2003 <nischintd@mindfiresolutions.com>
2026-02-19 16:42:08 +0530

    feat: add bottle container to the animation, fix axis

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,11 +11,17 @@ import { BOTTLE_VARITIES, BUTTON, FRONT_SECTION, NAV_LINKS } from '@/config/cons
 import CommonBtn from './ui/CommonBtn'
 import RightBotol from '../../public/botol/bottle_right.svg'
 import LeftBotol from '../../public/botol/bottle_left.svg'
-import { motion } from 'motion/react'
+import { motion, useScroll, useTransform } from 'motion/react'
+import BotolCap from '../../public/botol/Bottle-Cap.svg'
+import BotolContainer from '../../public/botol/Bottle-down.svg'
 
 const Hero = () => {
+  const { scrollYProgress } = useScroll({})
   const varieties: string[] = BOTTLE_VARITIES
   const varieties_icon = [variety1, variety2, variety3, variety4]
+
+  const bottleTopY = useTransform(scrollYProgress, [0, 0.185], [-50, 1200])
+  const bottleBottomY = useTransform(scrollYProgress, [0, 0.185], [-350, 400])
 
   return (
     <div className="flex flex-col justify-center items-center mt-5">
@@ -36,8 +42,33 @@ const Hero = () => {
           <CommonBtn text={BUTTON.INQUIRY_NOW} />
         </div>
       </nav>
+      <div className="absolute inset-0 top-40 z-50 -translate-y-36 lg:-translate-y-0">
+        <motion.div
+          className="bottle-top"
+          initial={{ y: 200 }}
+          animate={{ y: -40 }}
+          transition={{ duration: 2.5, delay: 1 }}
+          style={{ y: bottleTopY }}
+        >
+          <Image src={BotolCap} alt="Botol Cap" className="m-auto scale-50 lg:scale-100" priority />
+        </motion.div>
+        <motion.div
+          className="bottle-bottom"
+          initial={{ y: -600 }}
+          animate={{ y: -350 }}
+          transition={{ duration: 2.5, delay: 1 }}
+          style={{ y: bottleBottomY }}
+        >
+          <Image
+            src={BotolContainer}
+            alt="Bottle Bottom"
+            className="m-auto scale-50 lg:scale-100"
+            priority
+          />
+        </motion.div>
+      </div>
       <motion.div
-        className="circle bg-gradient-to-b from-[#00D1FF] to-[#1A83FF] h-[600px] w-[600px] rounded-full p-2 absolute top-40"
+        className="circle bg-gradient-to-b from-[#4DFBFB] to-[#788EFF] h-[600px] w-[600px] rounded-full p-2 absolute top-45"
         initial={{ scale: 0.6 }}
         animate={{ scale: 1 }}
         transition={{ delay: 1, duration: 2 }}
@@ -49,8 +80,8 @@ const Hero = () => {
           transition={{ delay: 1, duration: 2 }}
         ></motion.div>
       </motion.div>
-      <div className="mt-24 z-40 h-screen">
-        <div className="flex flex-col items-center mt-8">
+      <div className="mt-46 z-40">
+        <div className="flex flex-col items-center">
           <motion.div
             initial={{ opacity: 0, y: -50, scale: 0 }}
             animate={{ opacity: 1, y: 0, scale: 1 }}
@@ -70,11 +101,11 @@ const Hero = () => {
           <div className="flex justify-evenly w-screen">
             <motion.div
               initial={{ opacity: 0, scale: 0, x: -220, y: 50 }}
-              animate={{ opacity: 1, scale: 1.4 }}
+              animate={{ opacity: 1, scale: 1.2 }}
               transition={{ duration: 3, delay: 1 }}
-              className="hidden lg:block"
+              className="hidden lg:block mt-15"
             >
-              <Image src={RightBotol} alt="Botol Left" priority />
+              <Image src={LeftBotol} alt="Botol Left" priority />
             </motion.div>
             <motion.button
               className="bg-gradient-to-r from-[#00D1FF] to-[#1A83FF] text-white text-xl mt-8 px-10 py-2 rounded-3xl h-12"
@@ -86,17 +117,17 @@ const Hero = () => {
             </motion.button>
             <motion.div
               initial={{ opacity: 0, scale: 0, x: 220, y: 50 }}
-              animate={{ opacity: 1, scale: 1.4 }}
+              animate={{ opacity: 1, scale: 1.2 }}
               transition={{ duration: 3, delay: 1 }}
-              className="hidden lg:block"
+              className="hidden lg:block mt-15"
             >
-              <Image src={LeftBotol} alt="Botol Right" priority />
+              <Image src={RightBotol} alt="Botol Right" priority />
             </motion.div>
           </div>
         </div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2  w-full">
+      <div className="grid grid-cols-1 lg:grid-cols-2 mt-72 w-full">
         <div className="bg-[#E6D4CB]">
           <Image src={BottleHand} alt="BottleHand" className="w-full h-full" />
         </div>
